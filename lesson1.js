df4df1d0b53caaefee0f29d4343441d6d6458ed0
Shivalove <shivalove.k.k@gmail.com>
2018-04-16 21:29:57 +0300

    newCommit

---
+++ lesson1.js
@@ -0,0 +1,35 @@
+console.log ('hello world');
+
+
+
+//console.log('line #5');
+var myName = 'Kirill';
+console.log (myName);
+var str = 'string' ; // это строчный тип данных 
+var number = 125.7;
+var boolean = true; // or false
+var someVar;
+
+console.log(someVar); // undefined
+
+somevar = null ;
+var file = null; 
+
+//primitives
+
+//do something file = 'url';
+
+console.log(file);
+
+//object
+
+var myObj1 = { name: 'Alex'};
+
+var newObj = myObj1;
+
+newObj.name = 'Vova';
+console.log(myObj1);
+console.log(typeof myObj1);
+ var myObj2 = Object.create(myObj1);
+ console.log(myObj2);
+ console.log(myObj1);
